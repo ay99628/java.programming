9b623368a133ad82bf56ba50020544f55e84dfba
ay99628 <2714443583@qq.com>
2024-09-22 18:36:52 +0800

    Add files via upload

---
+++ test8.java
@@ -0,0 +1,11 @@
+public class test8 {
+    public static void main(String[] args) {
+        double perimeter,area;
+        double radius= 5.5;
+        double pi=3.14;
+        perimeter=2*radius*pi;
+        area=radius*radius*pi;
+System.out.println(perimeter);
+System.out.println(area);
+    }
+}
