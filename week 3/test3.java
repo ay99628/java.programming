3c4c36cd6835695d899f77a2340d61dcade00dee
ay99628 <2714443583@qq.com>
2024-09-30 09:42:58 +0800

    Add files via upload

---
+++ week 3/test3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class test3 {
+    public static void main(String[] args) {
+        final double PI = 3.14159;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the radius of the circle: ");
+        double radius = input.nextDouble();
+        double area = PI * radius * radius;
+        System.out.println("The area of the circle of radius" +radius + " is " + area);
+    }
+}
---
+++ week 3/test4.java
@@ -0,0 +1,8 @@
+public class test4 {
+    public static void main(String[] args) {
+        double miles = 100;
+        final double kilometers_per_miles = 1.609;
+        double kilometers = miles * kilometers_per_miles;
+        System.out.println(kilometers);
+    }
+}
