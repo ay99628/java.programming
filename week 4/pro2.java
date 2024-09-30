0c100d28cd1ddc5c21ddab9ee16b91ec57be7422
ay99628 <2714443583@qq.com>
2024-09-30 11:05:23 +0800

    Add files via upload

---
+++ week 4/pro2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class pro2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int seconds = input.nextInt();
+        int minutes = seconds / 60;
+        int remainingSeconds = seconds % 60;
+        System.out.println("minutes = " + minutes);
+        System.out.println("remainingSeconds = " + remainingSeconds);
+    }
+}
