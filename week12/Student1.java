cf9515eda82b737f583664fd63c3cee63fc7e1b2
ay99628 <2714443583@qq.com>
2024-11-25 11:11:02 +0800

    Create Student1.java

---
+++ week12/Student1.java
@@ -0,0 +1,24 @@
+public class Student1 {
+    private String name;
+    private int age;
+    public Student1(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        if(age > 0){
+            this.age = age;
+        }else{
+            System.out.println("Invalid age!");
+        }
+    }
+}
