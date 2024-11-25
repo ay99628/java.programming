e3b2b6da8ea6450233245abe1881996b4b235389
ay99628 <2714443583@qq.com>
2024-11-25 23:11:05 +0800

    Create TestStudent2.java

---
+++ week12/TestStudent2.java
@@ -0,0 +1,9 @@
+//this keyword
+public class TestStudent2 {
+    public static void main(String[] args) {
+        Student2 stu1 = new Student2();
+        Student2 stu2 = new Student2();
+        System.out.println("The name of student1 is " + stu1.getName());
+        System.out.println("The name of student2 is " + stu2.getName());
+    }
+}
