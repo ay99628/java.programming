6a5b970c6fe1846d8cbea70b963218892a7ef74c
ay99628 <2714443583@qq.com>
2024-12-09 10:09:22 +0800

    Add files via upload

---
+++ week15/TestLoanClass.java
@@ -0,0 +1,19 @@
+package p1;
+import java.util.Scanner;
+public class TestLoanClass {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter annual interest rate,for example, 8.25: ");
+        double annualInterestRate = input.nextDouble();
+        System.out.print("Enter number of years as an integer: ");
+        int numberOfYear = input.nextInt();
+        System.out.print("Enter loan amount, for example,120000.95: ");
+        double loanAmount = input.nextDouble();
+        input.close();
+        i loan = new i(annualInterestRate,numberOfYear,loanAmount);
+
+        System.out.printf("The loan was created on %s\n" + "The monthly payment is %.2f\nThe total payment is %.2f\n",
+                loan.getLoanDate(),loan.getMonthlyPayment(),loan.getTotalPayment());
+    }
+
+}
